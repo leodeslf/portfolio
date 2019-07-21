dfe7d7b8c76af3b0713c514795132a12f2bf9280
Leo <leodesouzauy@hotmail.com>
2019-07-21 20:13:42 -0300

    first commit

---
+++ src/Portfolio.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+export default class Portfolio extends Component {
+  render() {
+    return (
+      <>
+        <header className="Portfolio-header">
+
+        </header>
+        <main className="Portfolio-main">
+
+        </main>
+        <footer className="Portfolio-footer">
+
+        </footer>
+      </>
+    )
+  }
+}

f9399f10fcc857eefead89eb756c240aaba5356f
Leo <leodesouzauy@hotmail.com>
2019-08-01 01:17:41 -0300

    use gh-pages

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
 import Portfolio from './Portfolio';
 import './sass/main.css';
 
-ReactDOM.render(<Portfolio />, document.getElementById('root'));
+render(<Portfolio />, document.getElementById('root'));

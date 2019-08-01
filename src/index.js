c6f1d26a02a815debd1aa0124594b53bf17d781a
Leo <leodesouzauy@hotmail.com>
2019-08-01 04:00:36 -0300

    first one

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { render } from 'react-dom';
+import Portfolio from './Portfolio';
+import './sass/main.css';
+
+render(<Portfolio />, document.getElementById('root'));

969a9c9674af7ee70fa78f363d3ef4c9853937fc
Leo <leodesouzauy@hotmail.com>
2020-07-05 19:56:23 -0300

    cv link

--- src/components/Cv.js
+++ src/components/Cv.js
@@ -6,7 +6,7 @@ export default function Cv() {
       <h2>Currículum Vitae</h2>
       <p>
         Para más información personal, educación formal, informal y otras cosas
-        poco interesantes, ver y descargar <a href="./portfolio/Leonardo-de-Souza-Leal-Figueira-CV.pdf">
+        poco interesantes, ver y descargar <a href="./Leonardo-de-Souza-Leal-Figueira-CV.pdf">
           mi curriculum aquí</a>.
       </p>
     </section>

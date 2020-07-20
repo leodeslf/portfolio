bce17f21b8f5dd1eaebadc7fe3b704f9d69377d5
Leo <leodesouzauy@hotmail.com>
2020-07-20 04:54:43 -0300

    css naming, page naming, manifest update

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Title from './components/Title.js';
 import About from './components/About.js';
 import Tools from './components/Tools.js';
@@ -6,25 +6,23 @@ import Projects from './components/Projects.js';
 import Cv from './components/Cv.js';
 import Contact from './components/Contact.js';
 import Nav from './components/Nav.js';
-import ColorSchemeSwitch from './components/ColorSchemeSwitch.js';
+import DarkMode from './components/DarkMode.js';
 
-export default class Portfolio extends Component {
-  render() {
-    return (
-      <>
-        <main className="Portfolio">
-          <Title />
-          <About />
-          <Tools />
-          <Projects />
-          <Cv />
-          <Contact />
-        </main>
-        <aside>
-          <Nav />
-          <ColorSchemeSwitch />
-        </aside>
-      </>
-    );
-  }
+export default function Portfolio() {
+  return (
+    <>
+      <main className="Portfolio">
+        <Title />
+        <About />
+        <Tools />
+        <Projects />
+        <Cv />
+        <Contact />
+      </main>
+      <aside>
+        <Nav />
+        <DarkMode />
+      </aside>
+    </>
+  );
 }
--- src/components/DarkMode.js
+++ src/components/DarkMode.js
@@ -1,41 +1,56 @@
 import React, { Component } from 'react'
 
-export default class ColorSchemeSwitch extends Component {
+export default class DarkMode extends Component {
   constructor() {
     super();
     this.state = {
+      // Get current state of color scheme
       isDark: localStorage.getItem('colorScheme') === 'dark'
     }
   }
 
   componentDidMount() {
+    // Sets current state of color scheme (if it exists)
     if (this.state.isDark) document.body.classList.add('dark');
   }
 
   resetScheme(e) {
+    // True = dark mode active
     const checked = e.target.checked;
     if (checked) {
+      /**
+       * If used:
+       * - Add class for CSS
+       * - Save local config
+       */
       document.body.classList.add('dark');
       localStorage.setItem('colorScheme', 'dark');
     }
     else {
+      /**
+       * If not:
+       * - Remove class for CSS
+       * - Delete local config
+       */
       document.body.classList.remove('dark');
       localStorage.removeItem('colorScheme');
     }
+    // Finally update state
     this.setState({ isDark: checked });
   }
 
   render() {
     return (
-      <span className="color-scheme-switch">
+      <span className="dark-mode">
         <input
-          className="color-scheme-switch__input"
-          id="css" type="checkbox"
+          id="mode"
+          className="dark-mode__input"
+          type="checkbox"
           defaultChecked={this.state.isDark}
           onChange={e => this.resetScheme(e)} />
         <label
-          className="color-scheme-switch__label"
-          htmlFor="css" title="Switch color scheme">
+          className="dark-mode__label"
+          htmlFor="mode" title="Cambiar modo de color">
         </label>
       </span>
     );

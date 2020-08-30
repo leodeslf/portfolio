54f55bc34271bad6cd3166517bccedaf671acd38
Leo <leodesouzauy@hotmail.com>
2020-08-30 15:04:28 -0300

    fix last  link missed

--- src/js/utils.js
+++ src/js/utils.js
@@ -1,9 +1,10 @@
-const MARGIN = 192;
+const MARGIN = window.innerHeight * .5;
 let titles = undefined;
 let steps = undefined;
 
 window.addEventListener('load', () => {
-  titles = document.getElementsByTagName('h2');
+  titles = document.querySelectorAll('h1, h2');
+  console.log(titles)
   steps = document.querySelectorAll('.step');
 
   steps.forEach(step => {
@@ -16,7 +17,6 @@ window.addEventListener('load', () => {
 
   // Check current state.
   setActiveStep();
-  console.log('loaded');
 });
 
 // Check for the active step.
@@ -32,8 +32,13 @@ const setActiveStep = () => {
 
   // If not, find the active one.
   for (let i = 0; i < titles.length; i++) {
-    // If the next tilte is too low, current is the active one.
-    if (titles[i + 1] && titles[i + 1].offsetTop - MARGIN > SCROLLED) {
+    // Check if current (i) is the active one.
+    if (
+      // The next one is too low (and it exists) or...
+      (titles[i + 1] && titles[i + 1].offsetTop - MARGIN > SCROLLED) ||
+      // It (i) is the last one.
+      (i === titles.length - 1)
+    ) {
       // Finally, add and remove the 'active' class respectively.
       for (let j = 0; j < titles.length; j++) {
         if (j === i) steps[j].classList.add('step--active');

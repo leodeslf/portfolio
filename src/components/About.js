42347eb49087f26d6e6a091702433d76c5eb8d71
Leo <leodesouzauy@hotmail.com>
2020-08-31 21:16:02 -0300

    update content

--- src/components/About.js
+++ src/components/About.js
@@ -1,29 +1,16 @@
-import React, { Suspense, lazy } from 'react';
-
-const SeeMore = lazy(() => import( /* webpackChunkName: "seemore" */ './SeeMore'));
+import React from 'react';
 
 export default function About() {
   return (
     <section id="about" className="portfolio__about portfolio__elem">
-      <h1 title="Leonardo de Souza Leal Figueira">
-        Leonardo de Souza Leal Figueira
-      </h1>
+      <h2>Sobre mí</h2>
       <p>
-        <strong>Aprendiendo Desarrollo Frontend</strong>, como autodidacta,
-        estudio cómo <strong>brindamos información</strong> y <strong>creamos
-        experiencias</strong> en la web.
+        Sin saber a qué área del desarrollo o diseño dedicarme, casi
+        por accidente, descubrí el rol de Frontend. Desde entonces
+        aprendí conceptos para <strong>facilitar </strong> el
+        desarrollo a compañeros de equipo y <strong>
+          enriquecer</strong> la experiencia del usuario final.
       </p>
-      <Suspense fallback="">
-        <SeeMore mod={'about'} children={
-          <p className="see-more__hidden-block">
-            Sin saber a qué área del software o diseño dedicarme, casi por accidente,
-            descubrí el rol de Frontend. Desde entonces he aprendido conceptos
-            y herramientas que puedan <strong>favorecer</strong> a compañeros
-            de equipo durante el desarrollo y <strong>facilitar</strong> la
-            experiencia del usuario final (y continuaré haciéndolo).
-          </p>
-        } />
-      </Suspense>
     </section>
   );
 }

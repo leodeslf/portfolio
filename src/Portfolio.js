c12ed63fc59eee6215779ac918237bbefc0411a9
Leo <leodesouzauy@hotmail.com>
2020-09-05 17:10:49 -0300

    update intro

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -16,11 +16,12 @@ export default function Portfolio() {
       <main className="portfolio">
         <section id="start" className="portfolio__start portfolio__elem">
           <h1 title="Leonardo de Souza Leal Figueira">
-            Leonardo de Souza Leal Figueira
+            Leonardo de S. Leal Figueira
           </h1>
           <p>
-            Aprendiendo <strong>Frontend</strong> como autodidacta,
-            principalmente con <strong>React</strong> y <strong>SASS</strong>.
+            Aprendiendo <strong>Frontend</strong> como autodidacta,<br />
+            con <strong>React</strong> y <strong>SASS</strong> en
+            mi caja de herramientas,<br /> donde aún hay mucho lugar disponible.
           </p>
         </section>
         <About />

49330f682aaa29de811b97e346ce8cc479983ff7
Leo <leodesouzauy@hotmail.com>
2020-09-05 17:12:24 -0300

    update layout

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -7,19 +7,19 @@ export default function Contact() {
     <section id="contact" className="portfolio__contact portfolio__elem">
       <h2>Contacto</h2>
       <div className="contact__card">
-        <picture className="card__pic">
-          <source type="image/webp" srcSet={`${ORIGIN}.webp`} />
-          <img src={`${ORIGIN}.gif`} alt={`${ORIGIN}`} title="A human been" />
-        </picture>
         <ul className="card__data">
           <li>Montevideo, UY</li>
           <li>+598 99 705 972</li>
           <li>leodeslf@gmail.com</li>
           <li>
-            <a href="https://github.com/leodeslf">GitHub</a> / <a
-              href="https://www.linkedin.com/in/leodeslf/">LinkedIn</a>
+            <a href="github.com/leodeslf">GitHub</a> / <a
+              href="linkedin.com/in/leodeslf">LinkedIn</a>
           </li>
         </ul>
+        <picture className="card__pic">
+          <source type="image/webp" srcSet={`${ORIGIN}.webp`} />
+          <img src={`${ORIGIN}.gif`} alt={`${ORIGIN}`} title="A human been" />
+        </picture>
       </div>
     </section>
   );

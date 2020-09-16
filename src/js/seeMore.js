fabe0bfe3110bd14e1063c63d5a61ba4047fc775
Leo <leodesouzauy@hotmail.com>
2020-09-15 22:07:52 -0300

    swap from component to obj

---
+++ src/js/seeMore.js
@@ -0,0 +1,14 @@
+export default function seeMore(module) {
+  return {
+    input: {
+      id: `see-more__input--${module}`,
+      type: "checkbox",
+      className: "see-more__input",
+      defaultChecked: false
+    },
+    label: {
+      htmlFor: `see-more__input--${module}`,
+      className: "see-more__label text--small"
+    }
+  }
+}

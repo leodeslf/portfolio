ac4c2737929aacb656251592c0403804d9afed8e
Leo <leodesouzauy@hotmail.com>
2020-10-24 21:53:56 -0300

    refactor scroll/navLinks

--- src/js/scroll.js
+++ src/js/scroll.js
@@ -1,42 +1,34 @@
-const PADDING = 128; // 16 * 6
+const MARGIN = 64; // 16 * 4
+let distToScroll = [];
 let titles = undefined;
-let steps = undefined;
+let links = undefined;
 
 window.addEventListener('load', () => {
   titles = document.querySelectorAll('h1, h2');
-  steps = document.querySelectorAll('.step');
+  links = document.querySelectorAll('a.step');
 
-  steps.forEach(step => {
-    // Listen to clicks.
-    step.addEventListener('click', setActiveStep());
-  });
+  window.addEventListener('scroll', findActiveStep);
 
-  // Listen to scroll.
-  window.addEventListener('scroll', setActiveStep);
-
-  // Check current state.
-  setActiveStep();
+  findActiveStep();
 });
 
-// Check for the active step.
-const setActiveStep = () => {
+const findActiveStep = () => {
   const SCROLLED = document.documentElement.scrollTop;
+  let active = 0;
 
-  // If not, find the active one.
-  for (let i = 0; i < titles.length; i++) {
-    // Check if current (i) is the active one.
-    if (
-      // The next one is too low (and it exists) or...
-      (titles[i + 1] && titles[i + 1].offsetTop - PADDING > SCROLLED) ||
-      // It (i) is the last one.
-      (i === titles.length - 1)
-    ) {
-      // Finally, add and remove the 'active' class respectively.
-      for (let j = 0; j < titles.length; j++) {
-        if (j === i) steps[j].classList.add('step--active');
-        else steps[j].classList.remove('step--active');
-      }
-      return;
-    }
-  }
+  // Find dintances relative to the scroll to top.
+  titles.forEach((title, i) => {
+    distToScroll[i] = title.offsetTop - MARGIN - SCROLLED;
+  });
+
+  // Find the ones that are on 'range' / 'focus'.
+  distToScroll.forEach((dist, i) => {
+    if (dist <= 0) active = i;
+  });
+
+  links.forEach((step, i) => {
+    i === active ?
+      step.classList.add('step--active') :
+      step.classList.remove('step--active');
+  });
 }

6959f6f56ad9fac4913a1dce40c69ce7df660157
Leo <leodesouzauy@hotmail.com>
2020-12-10 15:13:49 -0300

    update logo

--- src/components/CopyRight.js
+++ src/components/CopyRight.js
@@ -1,9 +1,22 @@
 import React from 'react';
 
+const URL = "/icons/favicon-16x16.png";
+
 export default function CopyRight() {
   return (
     <footer className="copy-right">
-      © 2020 Leo de S.L.F
+      <p>
+        <span className="logo">
+          <img className="logo__icon"
+            src={URL}
+            alt="leodeslf logo"
+            width="16px"
+            height="16px" />
+          <span className="logo__text">
+            leodeslf © 2020
+          </span>
+        </span>
+      </p>
     </footer>
   );
 }

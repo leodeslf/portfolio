fe8e8a9e6c845177311b3cec547fd0f8ea514546
Leo <leodesouzauy@hotmail.com>
2020-12-15 21:11:27 -0300

    fix email input

--- src/components/Connect.js
+++ src/components/Connect.js
@@ -5,22 +5,25 @@ const email = 'leodeslf@gmail.com';
 
 export default function Connect() {
   return (
-    <section id="contact" className="portfolio__contact portfolio__elem">
+    <section id="connect" className="portfolio__connect portfolio__elem">
       <h2>Conectemos</h2>
-      <div className="contact__card">
+      <div className="connect__card">
         <ul className="card__data">
           <li>Montevideo, UY</li>
           <li>+598 99 705 972</li>
           <li title="Copiar al portapapeles.">
-            <input className="input-to-copy"
-              readOnly
-              aria-disabled
-              type="text"
-              value={email}
-              onClick={e => {
-                e.target.select();
-                document.execCommand('copy');
-              }} />
+            <label htmlFor="email">
+              <input className="input-to-copy"
+                id="email"
+                readOnly
+                aria-disabled
+                type="text"
+                value={email}
+                onClick={e => {
+                  e.target.select();
+                  document.execCommand('copy');
+                }} />
+            </label>
           </li>
           <li>
             <a href="https://github.com/leodeslf">GitHub</a> / <a

1f0df5aacfce0cedb5fab449c0a87c06843fd428
Leo <leodesouzauy@hotmail.com>
2020-12-20 18:07:03 -0300

    update workbox config

--- workbox-config.js
+++ workbox-config.js
@@ -1,31 +1,7 @@
 module.exports = {
-  globDirectory: "build/",
+  globDirectory: 'build/',
   globPatterns: [
-    "**/*.{xml,ico,png,svg,webp,html,json,pdf,txt}"
+    '**/*.{ico,html,json,svg,webp,txt,xml}'
   ],
-  swDest: "build/sw.js",
-  runtimeCaching: [
-    {
-      urlPattern: /\.(?:png|svg|webp)$/,
-      handler: 'CacheFirst',
-      options: {
-        cacheName: 'images',
-        expiration: {
-          maxAgeSeconds: 60 * 60 * 24 * 60,
-          maxEntries: 99
-        }
-      }
-    },
-    {
-      urlPattern: /\.(?:pdf|txt|xml)$/,
-      handler: 'CacheFirst',
-      options: {
-        cacheName: 'secondary',
-        expiration: {
-          maxAgeSeconds: 60 * 60 * 24 * 60,
-          maxEntries: 99
-        }
-      }
-    }
-  ],
+  swDest: 'build/sw.js',
 };

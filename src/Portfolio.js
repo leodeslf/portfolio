62a906d56790ca86d9ca6856ab600e2c06486d32
Leo <leodesouzauy@hotmail.com>
2021-02-13 20:37:17 -0300

    update contents

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -1,32 +1,24 @@
 import React, { lazy, Suspense } from 'react';
 
 const ROOT = './components/';
-
 const About = lazy(() => import(
   /* webpackChunkName: "about", webpackPreload: true */
   ROOT + 'About.js'));
-
 const Tools = lazy(() => import(
   /* webpackChunkName: "tools", webpackPreload: true */
   ROOT + 'Tools.js'));
-
 const Projects = lazy(() => import(
   /* webpackChunkName: "projects", webpackPreload: true */
   ROOT + 'Projects.js'));
-
 const Cv = lazy(() => import(
   /* webpackChunkName: "cv", webpackPreload: true */
   ROOT + 'Cv.js'));
-
 const Connect = lazy(() => import(
   /* webpackChunkName: "connect", webpackPreload: true */
   ROOT + 'Connect.js'));
-
 const Aside = lazy(() => import(
   /* webpackChunkName: "aside", webpackPreload: true */
   ROOT + 'Aside.js'));
-
-const displayName = "Leonardo de S. Leal";
 
 export default function Portfolio() {
   return (
@@ -34,17 +26,18 @@ export default function Portfolio() {
       <main className="portfolio">
         <section id="start" className="portfolio__start portfolio__elem">
           <h1 title="Leonardo de Souza Leal Figueira">
-            {displayName}
+            Leonardo de S. Leal
           </h1>
           <p>
-            Aprendiendo y especializándome en <strong>Desarrollo Frontend</strong>,<br />
-            con <strong>React</strong> y <strong>SASS</strong> como principales herramientas,<br />
-            acompañadas de <strong>Angular</strong>, <strong>Vue</strong> y <strong>TypeScript</strong>.
+            Especializándome en <strong>Desarrollo Frontend</strong>,
+            con <strong>React</strong> y <strong>SASS</strong> como
+            principales herramientas, acompañadas de <strong>Angular</strong>
+            , <strong>Vue</strong> y <strong>TypeScript</strong>.
           </p>
         </section>
         <About />
-        <Tools />
         <Projects />
+        <Tools />
         <Cv />
         <Connect />
       </main>

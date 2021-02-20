f54e96113571153a71bf06028e1f8fa9400a4dcb
Leo <leodesouzauy@hotmail.com>
2021-02-20 19:00:04 -0300

    move about to main component

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -1,15 +1,12 @@
 import React, { lazy, Suspense } from 'react';
 
 const ROOT = './components/';
-const About = lazy(() => import(
-  /* webpackChunkName: "about", webpackPreload: true */
-  ROOT + 'About.js'));
-const Tools = lazy(() => import(
-  /* webpackChunkName: "tools", webpackPreload: true */
-  ROOT + 'Tools.js'));
 const Projects = lazy(() => import(
   /* webpackChunkName: "projects", webpackPreload: true */
   ROOT + 'Projects.js'));
+const Tools = lazy(() => import(
+  /* webpackChunkName: "tools", webpackPreload: true */
+  ROOT + 'Tools.js'));
 const Cv = lazy(() => import(
   /* webpackChunkName: "cv", webpackPreload: true */
   ROOT + 'Cv.js'));
@@ -35,7 +32,21 @@ export default function Portfolio() {
             , <strong>Vue</strong> y <strong>TypeScript</strong>.
           </p>
         </section>
-        <About />
+        <section id="about" className="portfolio__about portfolio__elem">
+          <h2>Sobre mí</h2>
+          <p>
+            Con gusto por el Diseño y el Desarrollo, sin saber a cuál de esas áreas
+            dedicarme, mientras curioseaba sobre algunas tecnologías, -casi por
+            accidente- encontré el rol perfecto, un tal Frontend.
+          </p>
+          <p>
+            Desde entonces, aprendí herramientas
+            que permiten <strong>agilizar</strong> el
+            desarrollo, <strong>potenciar</strong> el trabajo en
+            equipo y <strong>enriquecer</strong> la experiencia
+            del usuario final.
+          </p>
+        </section>
         <Projects />
         <Tools />
         <Cv />

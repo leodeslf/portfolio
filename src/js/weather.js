af19469214840e00a62ef69efb186b2cdd99cf28
Leo <leodesouzauy@hotmail.com>
2021-02-20 18:56:09 -0300

    refactor weather func

--- src/js/weather.js
+++ src/js/weather.js
@@ -1,11 +1,9 @@
-const openweatherapi = [
+const OPEN_WEATHER_API = [
   'https://api.openweathermap.org/data/2.5/weather?',
   'appid=b280c897878592322aafe56701248929&',
   'lang=sp&units=metric&q=Montevideo,UY'
 ].join('');
 
 export async function fetchWeatherData() {
-  const res = await fetch(openweatherapi)
-  const jsonData = await res.json()
-  return jsonData;
+  return await (await fetch(OPEN_WEATHER_API)).json();
 }

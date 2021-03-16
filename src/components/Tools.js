28114df46f836aa078395ce1dc38d271515dc983
Leo <leodesouzauy@hotmail.com>
2021-03-16 08:32:11 -0300

    update styles

--- src/components/Tools.js
+++ src/components/Tools.js
@@ -5,10 +5,10 @@ export default function Tools() {
   return (
     <section id="tools" className="portfolio__tools portfolio__elem">
       <h2>Herramientas</h2>
-      <div className="tools__container">
+      <div className="tools__container text--small">
         {TOOLS.map(({ title, tool_list }, i) => (
-          <div key={i} className="tools__item">
-            <strong>{title}:</strong>
+          <div key={i} className="portfolio__tool-group">
+            <h2 className="h3">{title}</h2>
             <ul>
               {tool_list.map((item, j) => (
                 <li key={j}>{item}</li>

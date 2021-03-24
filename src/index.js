ce4dfdffb46cdaabe3dcccb934ac95637f56df63
Leo <leodesouzauy@hotmail.com>
2021-03-24 20:37:43 -0300

    remove unused import

--- src/index.js
+++ src/index.js
@@ -2,6 +2,5 @@ import React from 'react';
 import { render } from 'react-dom';
 import Portfolio from './Portfolio';
 import './sass/main.css';
-import './js/scroll';
 
 render(<Portfolio />, document.getElementById('root'));

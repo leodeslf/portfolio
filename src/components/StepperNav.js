856f75f1b65d4f70f126772c06868cd7885e6083
Leo <leodesouzauy@hotmail.com>
2021-03-24 20:35:57 -0300

    from class to functional component

--- src/components/StepperNav.js
+++ src/components/StepperNav.js
@@ -1,31 +1,29 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-const ANCHORS = [
-  ['#start', 'Inicio'],
-  ['#about', 'Sobre mí'],
-  ['#tools', 'Herramientas'],
-  ['#projects', 'Proyectos'],
-  ['#cv', 'Currículum Vitae'],
-  ['#connect', 'Conectar']
-];
+export default function StepperNav() {
+  const ANCHORS = [
+    ['#start', 'Inicio'],
+    ['#about', 'Sobre mí'],
+    ['#tools', 'Herramientas'],
+    ['#projects', 'Proyectos'],
+    ['#cv', 'Currículum Vitae'],
+    ['#connect', 'Conectar']
+  ];
 
-export default class StepperNav extends Component {
-  render() {
-    return (
-      <nav className="stepper text--small">
-        <ul className="stepper__list">
-          {ANCHORS.map((item, i) => (
-            <li className="stepper__item" key={i}>
-              <a
-                className="step"
-                href={item[0]}
-                onClick={() => window.location.replace(item[0])}>
-                {item[1]}
-              </a>
-            </li>
-          ))}
-        </ul>
-      </nav>
-    );
-  }
+  return (
+    <nav className="stepper text--small">
+      <ul className="stepper__list">
+        {ANCHORS.map((item, i) => (
+          <li className="stepper__item" key={i}>
+            <a
+              className="step"
+              href={item[0]}
+              onClick={() => window.location.replace(item[0])}>
+              {item[1]}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
 }

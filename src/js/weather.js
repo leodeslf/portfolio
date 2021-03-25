8619615526b20bf5bbc938db8b18f23797cb7264
Leo <leodesouzauy@hotmail.com>
2021-03-25 16:25:42 -0300

    refactor weather data module to single request

--- src/js/weather.js
+++ src/js/weather.js
@@ -4,6 +4,26 @@ const OPEN_WEATHER_URL = [
   'lang=sp&units=metric&q=Montevideo,UY'
 ].join('');
 
-export async function fetchWeatherData() {
-  return await (await fetch(OPEN_WEATHER_URL)).json();
+let weatherData = false;
+
+export default function weatherDataProvider() {
+  return weatherData;
+};
+
+async function fetchWeatherData() {
+  const data = await (await fetch(OPEN_WEATHER_URL)).json();
+  return data.cod === 200 ? {
+    name: data.name,
+    countryCode: data.sys.country,
+    temp: Math.round(data.main.temp),
+    tempMax: Math.round(data.main.temp_max),
+    tempMin: Math.round(data.main.temp_min),
+    text: data.weather[0].description,
+  } : false;
 }
+
+window.addEventListener('load', () => {
+  fetchWeatherData().then(res => {
+    if (res) weatherData = res;
+  });
+});

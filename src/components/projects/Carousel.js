20d9de0ea9b8c471565988ada1b6fa0c8a2dbfdf
Leonardo <leodesouzauy@gmail.com>
2021-08-11 22:10:16 -0300

    refactor projects loading

--- src/components/projects/Carousel.js
+++ src/components/projects/Carousel.js
@@ -6,15 +6,17 @@ const projectsN = PROJECTS.length - 1;
 
 export default function Carousel() {
   const [index, setIndex] = useState(0);
-  const projects = PROJECTS.map(item => <Project {...item} />);
   const goToPrev = () => setIndex(index - 1 > -1 ? index - 1 : projectsN);
   const goToNext = () => setIndex(index + 1 <= projectsN ? index + 1 : 0);
 
   return (
     <div className="projects__carousel">
-      {projects[index]}
+      <Project {...PROJECTS[index]} />
       <div className="carousel__controls">
-        <div className="control__prev" onClick={goToPrev}></div>
+        <div
+          className="control__prev"
+          onClick={goToPrev}
+        ></div>
         <div className="control__dots">
           {PROJECTS.map((_, i) =>
             <span
@@ -24,7 +26,10 @@ export default function Carousel() {
             ></span>
           )}
         </div>
-        <div className="control__next" onClick={goToNext}></div>
+        <div
+          className="control__next"
+          onClick={goToNext}
+        ></div>
       </div>
     </div>
   );

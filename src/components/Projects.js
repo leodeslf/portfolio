ea64250b00f22460bad40442e0db68559a4e02f2
Leonardo <leodesouzauy@gmail.com>
2021-08-13 22:58:34 -0300

    cleaning code && fix weather data initialization and fetching

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -5,16 +5,17 @@ import fetchWeatherData from './projects/weather';
 export const WeatherDataContext = createContext();
 
 export default function Projects() {
-  const [weatherData, setWeatherData] = useState(null);
+  const [weatherData, setWeatherData] = useState({ code: undefined });
 
   useEffect(() => {
-    fetchWeatherData().then(res => {
-      if (res === false || res) setWeatherData(res);
-    });
+    fetchWeatherData().then(setWeatherData);
   }, []);
 
   return (
-    <section id="projects" className="portfolio__projects portfolio__elem">
+    <section
+      id="projects"
+      className="portfolio__projects portfolio__elem"
+    >
       <h2>Proyectos</h2>
       <WeatherDataContext.Provider value={{ weatherData }}>
         <Carousel />

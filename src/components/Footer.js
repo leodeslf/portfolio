7f83bab19ab27eb76bc7355c41bfefb3f31f4491
Leonardo <leodesouzauy@gmail.com>
2021-08-13 23:37:32 -0300

    change font-size class

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer className="footer">
-      <p className="text--extra-small">
+      <p className="text--small">
         © 2021 leodeslf
       </p>
     </footer>

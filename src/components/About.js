c2ffb4b3e44d056b14a4dc86ecfbe1df08b51d5a
Leonardo <leodesouzauy@gmail.com>
2021-08-21 16:21:38 -0300

    Separate sections from main file

---
+++ src/components/About.js
@@ -0,0 +1,13 @@
+export default function About() {
+  return (
+    <section id="about" className="portfolio__about portfolio__elem">
+      <h2>Sobre mí</h2>
+      <p>
+        Siempre atraído tanto por el <em>Diseño</em> como por la <em>
+          Programación</em>, encontré casi por accidente el balance perfecto
+        entre ambas áreas: el Desarrollo Frontend. Desde entonces, me
+        dediqué a <em>aprender haciendo</em> como autodidacta.
+      </p>
+    </section>
+  );
+}
---
+++ src/components/Start.js
@@ -0,0 +1,13 @@
+export default function Start() {
+  return (
+    <section id="start" className="portfolio__start portfolio__elem">
+      <h1 title="Leonardo de Souza Leal Figueira">
+        <a href="./">Leonardo de S. Leal</a>
+      </h1>
+      <p className="start__description">
+        Entusiasta del <strong>Frontend</strong>, enfocado
+        en <strong>React</strong> y <strong>SASS</strong>.
+      </p>
+    </section>
+  );
+}

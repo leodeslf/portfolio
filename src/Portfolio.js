3761cf85b0470fa3f2ca3b37e7dec31d726bdb4b
Leonardo <leodesouzauy@gmail.com>
2021-08-21 16:25:22 -0300

    Remove sections from main file

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -1,3 +1,5 @@
+import Start from './components/Start';
+import About from './components/About';
 import Projects from './components/Projects';
 import Tools from './components/Tools';
 import Cv from './components/Cv';
@@ -9,24 +11,8 @@ export default function Portfolio() {
   return (
     <>
       <main className="portfolio">
-        <section id="start" className="portfolio__start portfolio__elem">
-          <h1 title="Leonardo de Souza Leal Figueira">
-            <a href="./">Leonardo de S. Leal</a>
-          </h1>
-          <p className="start__description">
-            Entusiasta del <strong>Frontend</strong>, enfocado
-            en <strong>React</strong> y <strong>SASS</strong>.
-          </p>
-        </section>
-        <section id="about" className="portfolio__about portfolio__elem">
-          <h2>Sobre mí</h2>
-          <p>
-            Siempre atraído tanto por el <em>Diseño</em> como por la <em>
-              Programación</em>, encontré casi por accidente el balance perfecto
-            entre ambas áreas: el Desarrollo Frontend. Desde entonces, me
-            dediqué a <em>aprender haciendo</em> como autodidacta.
-          </p>
-        </section>
+        <Start />
+        <About />
         <Projects />
         <Tools />
         <Cv />

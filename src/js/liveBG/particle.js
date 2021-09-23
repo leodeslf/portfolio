ceddeda154bda670159f50d98eeacafd5e9c686b
Leonardo <leodesouzauy@gmail.com>
2021-09-23 13:31:31 -0300

    Update live bg particles and rendering.

--- src/js/liveBG/particle.js
+++ src/js/liveBG/particle.js
@@ -1,7 +1,7 @@
 import { Vec2 } from '../../js/vec.min';
 
 const PI = 3.1415;
-const step = 0.25;
+const step = 0.225;
 
 export default class Particle {
   constructor(minX, minY, maxX, maxY, size) {
@@ -10,7 +10,7 @@ export default class Particle {
     this.lim = { minX, minY, maxX, maxY };
     this.vel = Vec2.random();
     this.vel.y *= .1;
-    this.vel.limitMinMagnitude(.6);
+    this.vel.magnitude = .5 + Math.random() * .25;
     this.size = size;
   }
   update() {
@@ -20,7 +20,7 @@ export default class Particle {
       this.vel.y *= -1;
 
     this.rev += step;
-    if (this.rev >= PI * 200) this.rev = 0;
+    if (this.rev >= PI * 2000) this.rev = 0; // Avoid big numbers.
     this.pos.add(this.vel);
   }
 }
--- src/js/liveBG/setup.js
+++ src/js/liveBG/setup.js
@@ -1,90 +1,97 @@
 import Particle from "./particle.js";
 
 // General state.
-const canvasArray = [];
-const ctxArray = [];
-let w, h, debounceReset;
+let raf;
+let w, h;
+let debounceReset;
+let birdCanvas, firefliyCanvas;
+let birdCtx, fireflyCtx;
 
-// Particles state.
-const particles = 8;
+// Particles config.
+const particles = 32;
 const particleSystem = [];
-let particlesRAF;
-const birdColor = '#fffc';
-const fireflyColor = '#ff28';
+const birdColor = '#fff';
+const fireflyColor = '#ff0';
 
+// Init.
 export function initLiveBG() {
-  canvasArray[0] = document.getElementsByClassName('live-bg__canvas--birds')[0];
-  canvasArray[1] = document.getElementsByClassName('live-bg__canvas--fireflies')[0];
+  birdCanvas = document.getElementsByClassName('live-bg__canvas--birds')[0];
+  firefliyCanvas = document.getElementsByClassName('live-bg__canvas--fireflies')[0];
 
   resetDimentions();
   resetParticles();
-  drawParticles();
 
   // Resize canvas and particles at resize with debouncing.
   window.addEventListener('resize', () => {
     clearTimeout(debounceReset);
+
     debounceReset = setTimeout(() => {
       resetDimentions();
       resetParticles();
-    }, 1000)
+    }, 1000);
   });
+}
+
+// Draw.
+function drawParticles() {
+  birdCtx.clearRect(0, 0, w, h);
+  fireflyCtx.clearRect(0, 0, w, h);
+
+  for (let i = 0; i < particles; i++) {
+    particleSystem[i].update();
+    let { pos, rev, size } = particleSystem[i];
+
+    // Birds.
+    const wingW = 4 + 4 * size;
+    const innerWingH = Math.sin(rev) * (2 + 2 * size * .2);
+    const outerWingH = Math.sin(rev - .7) * (2 + 2 * size * .8);
+
+    birdCtx.translate(pos.x, pos.y);
+    birdCtx.beginPath();
+    birdCtx.moveTo(-wingW, outerWingH);
+    birdCtx.lineTo(-wingW * .6, innerWingH);
+    birdCtx.lineTo(0, 0);
+    birdCtx.lineTo(wingW * .6, innerWingH);
+    birdCtx.lineTo(wingW, outerWingH);
+    birdCtx.lineWidth = size;
+    birdCtx.stroke();
+    birdCtx.closePath();
+    birdCtx.translate(-pos.x, -pos.y);
+
+    // Fireflies.
+    const radius = size * Math.sin(rev * .25);
+
+    fireflyCtx.beginPath();
+    fireflyCtx.arc(pos.x, pos.y, radius >= 0 ? radius : 0, 0, 360);
+    fireflyCtx.fill();
+    fireflyCtx.closePath();
+  }
+
+  raf = requestAnimationFrame(drawParticles);
+}
+
+// Util.
+function resetParticles() {
+  cancelAnimationFrame(raf);
+
+  for (let i = 0; i < particles; i++) {
+    particleSystem[i] = new Particle(0, 0, w, h, Math.random());
+  }
+
+  drawParticles();
 }
 
 function resetDimentions() {
   w = window.innerWidth;
   h = window.innerHeight;
-  canvasArray.forEach((canvas, i) => {
-    canvas.width = w;
-    canvas.height = h;
-    ctxArray[i] = canvas.getContext('2d');
-  });
-  ctxArray[0].strokeStyle = birdColor;
-  ctxArray[0].fillStyle = birdColor;
-  ctxArray[1].fillStyle = fireflyColor;
-}
 
-function resetParticles() {
-  cancelAnimationFrame(particlesRAF);
-  for (let i = 0; i < particles; i++) {
-    particleSystem[i] = new Particle(0, 0, w, h, 4 + Math.random() * 4);
-  }
-  initParticlesAnimation();
-}
+  birdCanvas.width = w;
+  birdCanvas.height = h;
+  birdCtx = birdCanvas.getContext('2d');
+  birdCtx.strokeStyle = birdColor;
 
-function initParticlesAnimation() {
-  for (let i = 0; i < particles; i++) {
-    particleSystem[i].update();
-  }
-  particlesRAF = requestAnimationFrame(initParticlesAnimation);
-}
-
-function drawParticles() {
-  ctxArray[0].clearRect(0, 0, w, h);
-  ctxArray[1].clearRect(0, 0, w, h);
-  for (let i = 0; i < particles; i++) {
-    let { pos, rev, size } = particleSystem[i];
-    size *= 1
-
-    // Birds.
-    const innerY = Math.sin(rev);
-    const outerY = Math.sin(rev - .8);
-    ctxArray[0].translate(pos.x, pos.y);
-    ctxArray[0].beginPath();
-    ctxArray[0].moveTo(-size, outerY * size * .7);
-    ctxArray[0].lineTo(-size * .5, innerY * size * 0.25);
-    ctxArray[0].lineTo(0, 0);
-    ctxArray[0].lineTo(size * .5, innerY * size * 0.25);
-    ctxArray[0].lineTo(size, outerY * size * .7);
-    ctxArray[0].stroke();
-    ctxArray[0].beginPath();
-    ctxArray[0].translate(-pos.x, -pos.y);
-
-    // Fireflies.
-    const r = size * Math.sin(rev * .3) / 6;
-    ctxArray[1].beginPath();
-    ctxArray[1].arc(pos.x, pos.y, r >= 0 ? r : 0, 0, 360);
-    ctxArray[1].fill();
-    ctxArray[1].closePath();
-  }
-  requestAnimationFrame(drawParticles);
+  firefliyCanvas.width = w;
+  firefliyCanvas.height = h;
+  fireflyCtx = firefliyCanvas.getContext('2d');
+  fireflyCtx.fillStyle = fireflyColor;
 }
